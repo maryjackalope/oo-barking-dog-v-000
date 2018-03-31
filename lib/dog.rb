78801b1fdf6a1b8f6566af1927b9a0c9fa257440
Mary Jack <maryjackinthebox@gmail.com>
2018-03-31 00:43:20 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Dog
+  
+  def name=(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+  
+  def bark
+    puts "woof!" #this was seriously just because I had a lowercase w.
+  end 
+
+end #Dog
